277991241b0a45965473429b1e3b1bddfbf9976f
Kartik <hellokartikk@gmail.com>
2024-02-18 11:53:48 +0530

    Initial Commit

---
+++ Cricket/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Cricket
+//
+//  Created by kartik khorwal on 18/02/24.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ Cricket/CricketApp.swift
@@ -0,0 +1,17 @@
+//
+//  CricketApp.swift
+//  Cricket
+//
+//  Created by kartik khorwal on 18/02/24.
+//
+
+import SwiftUI
+
+@main
+struct CricketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
